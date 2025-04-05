f198944e87dc284884d04d76e040ebf16e2c2c13
Leliksey <captainfroody@gmail.com>
2025-04-05 22:54:21 +0300

    error 13

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link rel="stylesheet" href="{{ asset('build/assets/app-vXaJq8TV.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-nQMqOp12.css') }}">
     <title>Главная</title>
 </head>
 <body>
